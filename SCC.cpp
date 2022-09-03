0c5f29706cf468beba921ad082046e9673c67fb7
dong geon kim <66730120+dong-gas@users.noreply.github.com>
2022-09-04 01:17:32 +0900

    Update SCC.cpp

--- SCC.cpp
+++ SCC.cpp
@@ -1,3 +1,13 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define MOD 1000000007
+#define INF 1000000000000000000
+#define all(v) v.begin(), v.end()
+#define zip(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
 ll n, m, id, SN = 1;
 ll d[200020];
 ll sn[200020];
@@ -6,6 +16,8 @@ bool finished[200020];
 vector<ll> adj[200020];
 vector<vector<ll>> SCC;
 stack<ll> s;
+
+//찾은 SCC 순서 뒤집으면 위상정렬된 상태.
 
 ll dfs(ll x) {
     d[x] = ++id;
@@ -32,6 +44,11 @@ ll dfs(ll x) {
     return parent;
 }
 
+ll rev(ll x) { //2-sat에서 not 구하기
+	if (x <= n) return x + n;
+	return x - n;
+}
+
 void solve() {
     cin >> n >> m;
     while (m--) {
@@ -39,6 +56,25 @@ void solve() {
         adj[u].push_back(v);
     }
     for (ll i = 1; i <= n; i++) if (!d[i]) dfs(i);
+    
+    //여기서부터는 2-sat
+    //(a or b)면 !a->b, !b->a 추가해야 함.
+    for (ll i = 1; i <= n; i++) {
+		if (sn[i] == sn[i + n]) {//같은 SCC면 모순
+			cout << 0 << endl;
+			return;
+		}
+	}
+	cout << 1 << endl;
+	reverse(SCC.begin(), SCC.end()); //위상정렬
+	for (auto vec : SCC) {
+		for (auto now : vec) {//앞쪽부터 0을 먼저 채우면서 감.
+			if (ans[now]) continue;
+			ans[now] = 0;
+			ans[rev(now)] = 1;
+		}
+	}
+    
 }
 
 int main() {
