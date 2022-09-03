4e619365855133d2f5b1d71f7e57f17bd0d699ce
dong geon kim <66730120+dong-gas@users.noreply.github.com>
2022-09-04 01:10:01 +0900

    Create TEMPLATE.cpp

---
+++ TEMPLATE.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define MOD 1000000007
+#define INF 1000000000000000000
+#define all(v) v.begin(), v.end()
+#define zip(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+using namespace std;
+using ll = long long;
+using pll = pair<ll, ll>;
+
+void solve() {
+    
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    int tc = 1; //cin >> tc; 
+    while (tc--) solve();
+}
