9e174753d71f5ee3c21cd5e7102c245334408554
dong geon kim <66730120+dong-gas@users.noreply.github.com>
2022-09-05 16:26:30 +0900

    Update DINIC.cpp with struct

--- DINIC.cpp
+++ DINIC.cpp
@@ -1,24 +1,13 @@
-#include <bits/stdc++.h>
-#define endl '\n'
-#define MOD 1000000007
-#define INF 9876542310
-using namespace std;
-using ll = long long;
-using pll = pair<ll, ll>;
-
-const ll N = 450;
-
-ll n, f, d, x, y, k;
-ll cap[N][N];
-ll flow[N][N];
+struct edge {
+    ll to, cap, rev;
+};
 ll level[N];
 ll work[N];
-vector<ll> adj[N];
+vector<edge> adj[N];
 
 void add_edge(ll from, ll to, ll c) {
-    adj[from].push_back(to);
-    adj[to].push_back(from);
-    cap[from][to] = c;
+    adj[from].push_back({ to, c, (ll)adj[to].size() });
+    adj[to].push_back({ from, 0, (ll)adj[from].size() - 1 });
 }
 
 bool bfs(ll src, ll sink) {
@@ -29,12 +18,11 @@ bool bfs(ll src, ll sink) {
     queue<ll> q;
     q.push(src);
     while (!q.empty()) {
-        int now = q.front();
-        q.pop();
-        for (ll nxt : adj[now]) {
-            if (level[nxt] == -1 && cap[now][nxt] > flow[now][nxt]) {
-                level[nxt] = level[now] + 1;
-                q.push(nxt);
+        ll now = q.front(); q.pop();
+        for (auto& e : adj[now]) {
+            if (e.cap > 0 && level[e.to] == -1) {
+                level[e.to] = level[now] + 1;
+                q.push(e.to);
             }
         }
     }
@@ -44,12 +32,12 @@ bool bfs(ll src, ll sink) {
 ll dfs(ll now, ll sink, ll amount) {
     if (now == sink) return amount;
     for (ll& i = work[now]; i < adj[now].size(); i++) {
-        ll nxt = adj[now][i];
-        if (level[nxt] == level[now] + 1 && cap[now][nxt] - flow[now][nxt] > 0) {
-            ll df = dfs(nxt, sink, min(cap[now][nxt] - flow[now][nxt], amount));
+        auto e = adj[now][i];
+        if (e.cap > 0 && level[e.to] == level[now] + 1) {
+            ll df = dfs(e.to, sink, min(amount, e.cap));
             if (df > 0) {
-                flow[now][nxt] += df;
-                flow[nxt][now] -= df;
+                adj[now][i].cap -= df;
+                adj[e.to][e.rev].cap += df;
                 return df;
             }
         }
@@ -67,29 +55,4 @@ ll dinic(ll src, ll sink) {
         }
     }
     return max_flow;
-}
-
-void solve() {
-    cin >> n >> f >> d;
-    for (ll i = 1; i <= n; i++) {
-        cin >> x >> y;//f d
-        for (ll j = 1; j <= x; j++) {
-            cin >> k;
-            add_edge(2 * n + k, i, 1);
-        }
-        for (ll j = 1; j <= y; j++) {
-            cin >> k;
-            add_edge(n + i, 2 * n + f + k, 1);
-        }
-    }
-    for (ll i = 1; i <= n; i++) add_edge(i, i + n, 1);
-    for (ll i = 1; i <= f; i++) add_edge(0, 2 * n + i, 1);
-    for (ll i = 1; i <= d; i++) add_edge(2 * n + f + i, 2 * n + f + d + 1, 1);
-    cout << dinic(0, 2 * n + f + d + 1) << endl;
-}
-
-int main() {
-    cin.tie(0)->sync_with_stdio(0);
-    int tc = 1; //cin >> tc;
-    while (tc--) solve();
 }
