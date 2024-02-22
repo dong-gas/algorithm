4778912f93f064bc2111d812b7d6eac7b9d8d338
dong geon kim <66730120+dong-gas@users.noreply.github.com>
2024-02-22 17:58:43 +0900

    Update CONVOLUTION.cpp

--- MATH/CONVOLUTION.cpp
+++ MATH/CONVOLUTION.cpp
@@ -1,6 +1,21 @@
-//XOR
-//아래 코드를 실행하고 나면 a에 a와 b의 xor convolution이 저장됨.
-//a[i]에는 a[j]^b[k]이 i인 (j, k)쌍의 수가 정해짐. (j==k, j>k, j<k 다 세짐)
+void fwht_and(vector<ll> &a, bool inv) {
+    ll n = a.size();
+    ll dir = inv ? -1 : 1;
+    for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1)
+        for(ll l = 0; l < n; l += s)
+            for(ll i = 0; i < h; i++)
+                a[l + i] += dir * a[l + h + i];
+}
+
+void fwht_or(vector<ll> &a, bool inv) {
+    ll n = a.size();
+    ll dir = inv ? -1 : 1;
+    for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1)
+        for(ll l = 0; l < n; l += s)
+            for(ll i = 0; i < h; i++)
+                a[l + h + i] += dir * a[l + i];
+}
+
 void fwht_xor(vector<ll> &a, bool inv) {
     ll n = a.size();
     for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
@@ -15,31 +30,10 @@ void fwht_xor(vector<ll> &a, bool inv) {
     }
 }
 
-//a, b convolution
-fxht_xor(a,false);
-fxht_xor(b,false);
-for(int i=0;i<(1ll<<n);i++) a[i]*=b[i];
-fxht_xor(a,true);
-
-//a끼리 convolution
-fxht_xor(a,false);
-for(int i=0;i<(1ll<<n);i++) a[i]*=a[i];
-fxht_xor(a,true);
-
-//AND, OR도 같은 작업 하면 됨. 아래는 검증 필요.
-void fwht_and(vector<ll> &a, bool inv) {
-    ll n = a.size();
-    ll dir = inv ? -1 : 1;
-    for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1)
-        for(ll l = 0; l < n; l += s)
-            for(ll i = 0; i < h; i++)
-                a[l + i] += dir * a[l + h + i];
-}
-void fwht_or(vector<ll> &a, bool inv) {
-    ll n = a.size();
-    ll dir = inv ? -1 : 1;
-    for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1)
-        for(ll l = 0; l < n; l += s)
-            for(ll i = 0; i < h; i++)
-                a[l + h + i] += dir * a[l + i];
+vector<ll> convolution(vector<ll> a, vector<ll> b) {
+    fwht_xor(a, false); //and or xor만 수정
+    fwht_xor(b, false); //and or xor만 수정
+    for(ll i=0;i<(1ll<<n);i++) a[i]*=b[i];
+    fwht_xor(a, true);  //and or xor만 수정
+    return a;
 }
