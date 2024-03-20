13107fcfe034bd79123fa252e10e6071ed2a3e87
gum-bad <154876555+gum-bad@users.noreply.github.com>
2024-03-21 03:08:34 +0900

    Update MCMF.cpp

--- FLOW/MCMF.cpp
+++ FLOW/MCMF.cpp
@@ -1,8 +1,8 @@
 //O(Ef)
-const ll N = 2020, INF = 1e9;
-struct edge {
-	ll to, cap, rev, cost;
-};
+//reference: https://github.com/koosaga/DeobureoMinkyuParty/blob/master/teamnote.pdf
+const ll N = 222, INF = 1e9;
+
+struct edge { ll to, cap, rev, cost; };
 vector<edge> adj[N];
 
 ll dist[N], p[N], pe[N];
@@ -16,16 +16,18 @@ void add_edge(ll u, ll v, ll c, ll cost) {
 bool spfa(ll s, ll t) {
     fill(dist, dist + N, INF);
     memset(inQ, 0, sizeof(inQ));
-
+    
     queue<ll> q;
     q.emplace(s); dist[s] = 0; inQ[s] = 1;
-    while (q.size()) {
+    bool ok=0;
+    while (!q.empty()) {
         ll x = q.front(); q.pop();
+        if(x==t) ok=1;
         inQ[x] = 0;
 
         for (ll i = 0; i < adj[x].size(); i++) {
-            auto& e = adj[x][i];
-            if (e.cap && dist[x] + e.cost < dist[e.to]) {
+            auto e = adj[x][i];
+            if (e.cap > 0 && dist[x] + e.cost < dist[e.to]) {
                 dist[e.to] = dist[x] + e.cost;
                 p[e.to] = x; pe[e.to] = i;
                 if (!inQ[e.to]) {
@@ -36,22 +38,21 @@ bool spfa(ll s, ll t) {
         }
 
     }
-    return dist[t] != INF;
+    return ok;
 }
 
 pll mcmf(ll s, ll t) {
-    ll min_cost = 0, max_flow = 0, flow, rev;
+    ll min_cost = 0, max_flow=0, flow, rev;
     while (spfa(s, t)) {
         flow = INF;
-        for (ll i = t; i != s; i = p[i])
-            flow = min(flow, adj[p[i]][pe[i]].cap);
+        for (ll i = t; i != s; i = p[i]) flow = min(flow, adj[p[i]][pe[i]].cap);            
         min_cost += flow * dist[t];
         for (ll i = t; i != s; i = p[i]) {
             rev = adj[p[i]][pe[i]].rev;
             adj[p[i]][pe[i]].cap -= flow;
             adj[i][rev].cap += flow;
         }
+        max_flow+=flow;
     }
-    max_flow += flow;
     return { min_cost,max_flow };
 }
