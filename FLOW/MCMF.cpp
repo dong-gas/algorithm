a1adc68eaa5b8f77c84b5e76fb961441883b193f
dong-gas <66730120+dong-gas@users.noreply.github.com>
2025-05-14 16:18:50 +0900

    Update MCMF.cpp

--- FLOW/MCMF.cpp
+++ FLOW/MCMF.cpp
@@ -3,59 +3,66 @@
 
 //to do: https://justicehui.github.io/hard-algorithm/2020/03/24/effective-mcmf/
 
-const ll N = 222, INF = 1e9;
+using pint = pair<int, int>;
 
-struct edge { ll to, cap, rev, cost; };
+const int N = 606, INF = 1e9;
+
+struct edge {
+    int to, cap, rev, cost;
+};
 vector<edge> adj[N];
 
-ll dist[N], p[N], pe[N];
+int dist[N], p[N], pe[N];
 bool inQ[N];
 
-void add_edge(ll u, ll v, ll c, ll cost) {
-	adj[u].push_back({ v, c, (ll)adj[v].size(), cost });
-	adj[v].push_back({ u, 0, (ll)adj[u].size() - 1, -cost });
+void add_edge(int u, int v, int c, int cost) {
+    adj[u].push_back({v, c, (int) adj[v].size(), cost});
+    adj[v].push_back({u, 0, (int) adj[u].size() - 1, -cost});
 }
 
-bool spfa(ll s, ll t) {
+bool spfa(int s, int t) {
     fill(dist, dist + N, INF);
     memset(inQ, 0, sizeof(inQ));
-    
-    queue<ll> q;
-    q.emplace(s); dist[s] = 0; inQ[s] = 1;
-    bool ok=0;
+
+    queue<int> q;
+    q.emplace(s);
+    dist[s] = 0;
+    inQ[s] = 1;
+    bool ok = 0;
     while (!q.empty()) {
-        ll x = q.front(); q.pop();
-        if(x==t) ok=1;
+        int x = q.front();
+        q.pop();
+        if (x == t) ok = 1;
         inQ[x] = 0;
 
-        for (ll i = 0; i < adj[x].size(); i++) {
+        for (int i = 0; i < adj[x].size(); i++) {
             auto e = adj[x][i];
             if (e.cap > 0 && dist[x] + e.cost < dist[e.to]) {
                 dist[e.to] = dist[x] + e.cost;
-                p[e.to] = x; pe[e.to] = i;
+                p[e.to] = x;
+                pe[e.to] = i;
                 if (!inQ[e.to]) {
                     inQ[e.to] = 1;
                     q.emplace(e.to);
                 }
             }
         }
-
     }
     return ok;
 }
 
-pll mcmf(ll s, ll t) {
-    ll min_cost = 0, max_flow=0, flow, rev;
+pint mcmf(int s, int t) {
+    int min_cost = 0, max_flow = 0, flow, rev;
     while (spfa(s, t)) {
         flow = INF;
-        for (ll i = t; i != s; i = p[i]) flow = min(flow, adj[p[i]][pe[i]].cap);            
+        for (int i = t; i != s; i = p[i]) flow = min(flow, adj[p[i]][pe[i]].cap);
         min_cost += flow * dist[t];
-        for (ll i = t; i != s; i = p[i]) {
+        for (int i = t; i != s; i = p[i]) {
             rev = adj[p[i]][pe[i]].rev;
             adj[p[i]][pe[i]].cap -= flow;
             adj[i][rev].cap += flow;
         }
-        max_flow+=flow;
+        max_flow += flow;
     }
-    return { min_cost,max_flow };
+    return {min_cost, max_flow};
 }
